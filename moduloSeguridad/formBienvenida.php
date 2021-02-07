c731e45ce6f6f0fec564eeec75463d8ecb95c735
JuanEscurra <archys.wow@gmail.com>
2021-02-07 00:27:35 -0500

    m

---
+++ moduloSeguridad/formBienvenida.php
@@ -0,0 +1,42 @@
+<?php 
+	class formBienvenida{
+		public function formBienvenidaShow($Privilegios)
+		{
+			?>
+				<!DOCTYPE html>
+				<html>
+				<head>
+					<title>Formulario Bienvenida</title>
+					<link rel="stylesheet" href="../estilos/gestionarUsuarios.css">
+                	<link rel="stylesheet" href="../estilos/estilos_generales.css">
+                	<link rel="shorcut icon" type="image/x-icon" href="../img/ico   no.ico">
+					<link rel="shorcut icon" type="image/x-icon" href="../img/icono.ico">
+				    <link rel="stylesheet" href="../estilos/estilos_generales.css">
+				    <link rel="stylesheet" href="../estilos/estilos_formbienvenida.css">
+				</head>
+				<body>
+					<div class="logo">
+							<img src="../img/logo_header.png" height="100" width="230">
+							<form action="getUsuario.php" method="POST">
+								<input type="submit" name="btnCerrarSesion" value="Cerrar Sesión">
+							</form>
+					</div>
+				   <div class="div-div">
+						<div class="contenedor-privilegios">
+							<?php
+								for($i=0;$i<count($Privilegios);$i++){?>
+
+								<form class="form-url" method="POST" action="<?php echo $Privilegios[$i]['url'] ?>">
+            						<input class="input-campo" type="submit" name="<?php echo $Privilegios[$i]['privilegio'] ?>" value="<?php echo $Privilegios[$i]['nombre'] ?>">
+        						</form>
+        						<?php
+								}
+							?>
+						</div>
+					</div>
+				</body>
+				</html>
+			<?php 
+		}
+	}
+ ?>
